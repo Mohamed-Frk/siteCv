08fab9f5e540d539b770e9d9abcca8fa8f323245
Ferkioui <ferkioui.mohamed.cherif@gmail.com>
2020-01-04 23:09:33 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { AProposComponent } from './components/a-propos/a-propos.component';
+import { CompetencesComponent } from './components/competences/competences.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path : '', component : AccueilComponent },
+  { path : 'APropos', component : AProposComponent },
+  { path : 'Competences', component : CompetencesComponent },
+  { path : 'Experiences', component : ExperiencesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { AProposComponent } from './components/a-propos/a-propos.component';
+import { CompetencesComponent } from './components/competences/competences.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    AProposComponent,
+    CompetencesComponent,
+    ExperiencesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
